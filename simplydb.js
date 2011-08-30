aee64b82bd21865aff1a1e6d455bf7a218e598d5
Tim Marshall <timothyjmarshall@gmail.com>
2011-08-30 00:24:22 -0700

    Added .listDomains and fixed a few things

--- simplydb.js
+++ simplydb.js
@@ -8,34 +8,48 @@ function SimplyDB(accessKey, secret) {
 		dateOffset = null;
 	
 	if(accessKey === undefined) {
-		throw "No Access Key given";
+		throw 'No Access Key given';
 	}
 	if(secret === undefined) {
-		throw "No Secret Key given";
+		throw 'No Secret Key given';
 	}
 	
-	this.select = function(query, nextToken /* = '' */, consistent /* = true */) {
+	this.listDomains = function(callback, max /* = 100 */, nextToken /* = '' */) {
+		if(arguments.length > 1 && (+max < 1 || +max > 100)) {
+			throw 'Invalid MaxNumberOfDomains given';
+		}
 		makeRequest({
+			Action: 'ListDomains',
+			MaxNumberOfDomains: max,
+			NextToken: nextToken
+		}, callback);
+	};
+	
+	this.select = function(callback, query, nextToken /* = '' */, consistent /* = true */) {
+		makeRequest({
+			Action: 'Select',
 			SelectExpression: query,
-			ConsistentRead: arguments.length > 2 ? consistent : true,
-			Action: 'Select'
-		});
+			NextToken: nextToken,
+			ConsistentRead: arguments.length > 3 ? consistent : true
+		}, callback);
 	};
 	
 	function datetimeISO8601(date) {
-		return (date.getUTCFullYear() + '-' + (1 + date.getUTCMonth()) + '-' + (1 + date.getDate()) + 'T' + date.getUTCHours() + ':' + date.getUTCMinutes() + ':' + date.getUTCSeconds() + 'Z').replace(/\D\d\D/g, function(a, b) {	
+		return (date.getUTCFullYear() + '-' + (1 + date.getUTCMonth()) + '-' + (1 + date.getDate()) + 'T' + date.getUTCHours() + ':' + date.getUTCMinutes() + ':' + date.getUTCSeconds() + 'Z').replace(/\D\d{1}\D/g, function(a) {	
 			return a[0] + '0' + a.slice(1);
 		});
 	}
 	
-	function makeRequest(params, method /* = GET */) {
+	function makeRequest(params, callback, method /* = GET */) {
 		var
 			keys,
 			str,
 			i,
 			l,
 			parts = [],
-			queryStr;
+			queryStr,
+			sig,
+			tmp;
 		
 		method = method || 'GET';
 		params.Timestamp = datetimeISO8601(new Date());
@@ -49,35 +63,32 @@ function SimplyDB(accessKey, secret) {
 		});
 		
 		for(i = 0, l = keys.length; i < l; i++) {
-			if(params[keys[i]] === null) {
+			if((tmp = params[keys[i]]) === undefined || tmp === null) {
 				continue;
 			}
-			parts.push(keys[i] + '=' + urlEncode(params[keys[i]]));
+			parts.push(keys[i] + '=' + urlEncode(tmp));
 		}
-		queryStr = parts.join('&');
 		
-		params.Signature = crypto
+		queryStr = parts.join('&');
+		sig = crypto
 			.createHmac('SHA1', secret)
 			.update(method + '\nsdb.amazonaws.com\n/\n' + queryStr)
 			.digest('base64');
+		queryStr += '&Signature=' + urlEncode(sig);
 		
-		queryStr += '&Signature=' + urlEncode(params.Signature);
-		
-		console.log(params.Signature);
-		console.log('...');
+		console.log(queryStr);
+		console.log(params.Timestamp);
+		console.log(sig);
 		
 		https[method.toLowerCase()]({
 			host: 'sdb.amazonaws.com',
 			path: '/?' + queryStr
 		}, function(res) {
-			console.log("statusCode: ", res.statusCode);
-			console.log("headers: ", res.headers);
-			
-			res.on('data', function(d) {
-				console.log(d.toString('utf-8'));
+			res.on('data', function(data) {
+				callback(null, data.toString('utf-8'));
 			});
-		}).on('error', function(e) {
-			console.error(e);
+		}).on('error', function(err) {
+			callback(err);
 		});
 	}
 	
